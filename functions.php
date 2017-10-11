a66be6c6fa3a2fb430a98b37aa53a7789a8f31c9
Michael Goitein <mhgoten@gmail.com>
2017-10-11 12:37:12 -0400

    Added Child Theme JS
    Needed for sticky nav

--- functions.php
+++ functions.php
@@ -6,3 +6,9 @@ function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style') );
 }
+
+// Enqueue child them js file
+add_action( 'wp_enqueue_scripts', 'child_theme_js' );
+function child_theme_js() {
+  wp_enqueue_script( 'child-theme-js' , get_stylesheet_directory_uri() . '/child-theme-js.js' , array( 'twentyseventeen-global' ) , false , true );
+}
