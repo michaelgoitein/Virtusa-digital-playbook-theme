a9877e8408d577acaefb60f861ca55f76d25c3c4
Michael Goitein <mhgoten@gmail.com>
2017-10-17 12:08:18 -0400

    Closing space

--- template-parts/header/site-branding.php
+++ template-parts/header/site-branding.php
@@ -28,8 +28,8 @@
 			?>
 				<p class="site-description"><?php echo $description; ?></p>
 			<?php endif; ?>
+			<?php get_search_form(); ?>
 		</div><!-- .site-branding-text -->
-		<?php get_search_form(); ?>
 		<?php if ( ( twentyseventeen_is_frontpage() || ( is_home() && is_front_page() ) ) && ! has_nav_menu( 'top' ) ) : ?>
 		<a href="#content" class="menu-scroll-down"><?php echo twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ); ?><span class="screen-reader-text"><?php _e( 'Scroll down to content', 'twentyseventeen' ); ?></span></a>
 	<?php endif; ?>
