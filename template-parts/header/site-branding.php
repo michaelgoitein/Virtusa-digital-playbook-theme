1be4680a386ce54df578115c3e678d86d2d17519
Michael Goitein <mhgoten@gmail.com>
2017-10-17 12:11:25 -0400

    Fixing Search Position

--- template-parts/header/site-branding.php
+++ template-parts/header/site-branding.php
@@ -28,7 +28,7 @@
 			?>
 				<p class="site-description"><?php echo $description; ?></p>
 			<?php endif; ?>
-			<?php get_search_form(); ?>
+				<p class="search-form"><?php get_search_form(); ?></p>
 		</div><!-- .site-branding-text -->
 		<?php if ( ( twentyseventeen_is_frontpage() || ( is_home() && is_front_page() ) ) && ! has_nav_menu( 'top' ) ) : ?>
 		<a href="#content" class="menu-scroll-down"><?php echo twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ); ?><span class="screen-reader-text"><?php _e( 'Scroll down to content', 'twentyseventeen' ); ?></span></a>
